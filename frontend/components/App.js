3435f7046f59ef231f912d6cf23b57806b79a696
Michael Marconi <michaelziga2@gmail.com>
2023-08-04 14:04:50 -0500

    mapd

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,50 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: "Walk the Dog",
+          id: 1,
+          completed: false
+        },
+        {
+          name: "Garbage",
+          id: 2,
+          completed: false
+        },
+        {
+          name: "Laundry",
+          id: 3,
+          completed: false
+        }
+      ]
+    };
+  }
+
+  // Other methods and render function can be added here
+
+
   render() {
+    const { todos } = this.state
     return (
       <div>
-        Todo App
+        <h1>Todo App</h1>
+
+        <ul>
+          {
+            todos.map(todo => {
+              return (<li>{todo.name} {todo.completed?<span>- completed</span> : <span></span>}</li>)
+            })
+          }
+        </ul>
+        <form>
+          <input />
+          <button>Add</button>
+          </form>
+          <button>Clear</button>
       </div>
     )
   }
